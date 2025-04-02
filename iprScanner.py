2da8b50616d81d68b4ca18aaae1a69fbbc869dc4
modokashii <128351907+inoueseka@users.noreply.github.com>
2025-04-03 08:18:36 +0900

    Update iprScanner.py

--- iprScanner.py
+++ iprScanner.py
@@ -6,16 +6,13 @@ import re
 SEARCH_STRING = "Assets"
 
 for file_path in Path(".").rglob("*.ipr"):  
-    with file_path.open("rb") as file:  # Open file in binary mode
-        # Read the file
-        contents = file.read().decode(errors="ignore")  # Decode binary content to string
+    with file_path.open("rb") as file: 
+        contents = file.read().decode(errors="ignore") 
         
         if SEARCH_STRING in contents:
             print(file_path)
-            matches = re.findall(f'({SEARCH_STRING}.{{1,100}})', contents) # Find all occurrences of the search string following "Assets" up to specified bytes
-
-            # Alternatively, to find all occurrences of the search string preceding "Assets" up to specified bytes
-                # matches = re.findall(f'.{{1,100}}{SEARCH_STRING}.{{0,100}}', contents)
+            matches = re.findall(f'({SEARCH_STRING}.{{1,100}})', contents) 
+            # Alternatively, to find all occurrences of the search string preceding "Assets" up to specified bytes, do: matches = re.findall(f'.{{1,100}}{SEARCH_STRING}.{{0,100}}', contents)
             
             for match in matches:
                 print(match)
